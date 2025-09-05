4b79f5e7865072897e358f387a0773a442ec5b72
Ваше Имя <wee224225@gmail.com>
2025-09-05 09:58:05 +0300

    init commit

---
+++ src/test/java/school/gera/reservationsystem/ReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package school.gera.reservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
